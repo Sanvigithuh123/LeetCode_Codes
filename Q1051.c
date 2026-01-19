6db860a512c915329ac0ded0476ebcc5765809d3
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-01-19 11:15:08 +0530

    Implement heightChecker function to count mismatches

---
+++ Q1051.c
@@ -0,0 +1,27 @@
+int heightChecker(int* heights, int heightsSize) {
+    int temp,ans=0;
+    int expected[heightsSize];
+
+    for(int i=0;i<heightsSize;i++) {
+        expected[i]=heights[i];
+    }
+
+    for(int i=0;i<heightsSize-1;i++) {
+        for(int j=0;j<heightsSize-i-1;j++) {
+        if(expected[j]>expected[j+1]) {
+            temp=expected[j];
+            expected[j]=expected[j+1];
+            expected[j+1]=temp;
+        }
+        }
+    }
+ 
+    int count=0;
+
+   for(int i=0;i<heightsSize;i++) {
+    if(heights[i]!=expected[i]) {
+        count++;
+    }
+   }
+    return count;
+}
