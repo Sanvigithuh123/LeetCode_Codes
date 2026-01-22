ebf98f6ce400fcd6d5c4a626b4a32e1cde941645
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-01-22 10:06:35 +0530

    Add function to check for perfect numbers

---
+++ Q507.c
@@ -0,0 +1,12 @@
+bool checkPerfectNumber(int num) {
+    if(num<=1) return 0;
+    int sum=1;
+    for(int i=2;i*i<=num;i++) {
+        if(num%i==0) {
+        sum=sum+i;
+        if(i!=num/i)
+        sum=sum+num/i;
+        }
+    }
+    return sum==num;
+}
