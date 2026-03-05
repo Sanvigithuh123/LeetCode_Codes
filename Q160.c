a121d04be5fd24524e74455f0fd29988d210cb8d
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-03-05 23:09:37 +0530

    Implement function to find intersection of two lists

---
+++ Q160.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    struct ListNode *d1 = headA;
+    struct ListNode *d2 = headB;
+    while(d1!=d2){
+        if(d1==NULL) d1=headB;
+        else d1=d1->next;
+        if(d2==NULL) d2=headA;
+        else d2=d2->next;
+    }
+    return d1;
+}
