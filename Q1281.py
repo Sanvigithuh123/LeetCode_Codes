bf8697566032fe838af0795edaf65ae2c5370c99
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-03-15 10:36:05 +0530

    Implement method to subtract product and sum of digits

---
+++ Q1281.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        sum=0
+        prod=1
+        while n>0:
+            dig=n%10
+            sum=sum+dig
+            prod=prod*dig
+            n=n//10
+        return prod-sum
+        
