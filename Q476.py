ce557105f8c48cf59a7e9e915022ca266fc371aa
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-05-13 08:53:23 +0530

    Implement findComplement method in Solution class

---
+++ Q476.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        a=bin(num)[2:]
+        b=list(a)
+        for i in range(len(b)):
+            if b[i]=='1':
+                b[i]='0'
+            else:
+                b[i]='1'
+        c=''.join(b)
+        
+        return int(c,2)
+        
