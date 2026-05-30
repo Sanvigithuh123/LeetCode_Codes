94b2f8aaa19094ff9d8ac8e651d0e9e45c4c7e0d
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-05-30 10:00:53 +0530

    Implement canConstruct method in Solution class

---
+++ Q383.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for ch in ransomNote:
+            if ch not in magazine:
+                return False
+            magazine = magazine.replace(ch, "", 1)
+
+        return True
