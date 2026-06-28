8c31ace752c7862736e1bc9054a7917e3a0cdea0
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-06-28 09:57:33 +0530

    Add solution for maximum element after operations
    Implement a solution to find the maximum element after decrementing and rearranging the array.

---
+++ Q1846.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        maxi=1
+        for i in range(1,len(arr)):
+            if arr[i]>maxi:
+                maxi+=1
+        return maxi
+
+        
