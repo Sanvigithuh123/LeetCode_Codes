f27a06974fb374d6d47805ee2e227b6954e08b5d
Sanvi Mukherjee <sanvimukherjee07@gmail.com>
2026-08-27 21:47:47 +0530

    Implement mergeAlternately method in Solution class

---
+++ Q1768.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        a=[]
+        n=max(len(word1),len(word2))
+        for i in range(n):
+            if i<len(word1):
+                a.append(word1[i])
+            if i<len(word2):
+                a.append(word2[i])
+        return "".join(a)
+            
+        
